dcc96fb4ead24f5fe761f2436ff9432de266ee7d
Anar Arabli <anar.arabli@gmail.com>
2025-10-20 23:23:52 +0400

    Initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,47 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
-    }
+//    Scanner scan = new Scanner(System.in);
+//    System.out.println("Enter number : ");
+//    int ch = scan.nextInt();
+//
+//    String result ="";
+//
+//    if(ch%5==0) result += "Fizz";
+//    if(ch%3==0) result += "Buzz";
+//
+//    System.out.println(result);
+
+//------------------------------------------
+//    Scanner scan = new Scanner(System.in);
+//    System.out.println("Enter an integer :");
+//    int a = scan.nextInt();
+//
+//
+//    int[] numbers = new int[a];
+//
+//    for (int i=0; i<a; i++) {
+//        numbers[i] = scan.nextInt();
+//    }
+//    for (int i=0; i<a; i++) {
+//
+//        System.out.print(numbers[i]);
+//        if (i != numbers.length - 1) {
+//            System.out.print( ", ");
+//        }
+//    }
+//    System.out.println();
+//    System.out.println("Sorted : ");
+//    Arrays.sort(numbers);
+//    for (int i=0; i<a; i++) {
+//        System.out.print(numbers[i]);
+//        if (i != numbers.length - 1) {
+//            System.out.print( ", ");
+//        }
+//    }
+//-------------------------------------------------
+
+
+
 }
